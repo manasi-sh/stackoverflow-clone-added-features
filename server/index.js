834159313a4bdc7eb9514b1267feba37871b4612
manasi-sh <110293421+manasi-sh@users.noreply.github.com>
2024-01-05 19:46:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,27 +9,27 @@ import answerRoutes from "./routes/Answers.js";
 
 // import connectDB from "./connectMongoDb.js";
 
-dotenv.config();
+// dotenv.config();
 // connectDB();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// app.use('/',(req, res) => {
-//     res.send("This is a stack overflow clone API")
-// })
+app.use('/',(req, res) => {
+    res.send("This is a stack overflow clone API")
+})
 
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
 app.use("/answer", answerRoutes);
-app.get('/',(req,res)=>{
-    res.send("This is a stack overflow clone API")
-})
+// app.get('/',(req,res)=>{
+//     res.send("This is a stack overflow clone API")
+// })
 
 const PORT = process.env.PORT || 5000;
 
-const DATABASE_URL = process.env.CONNECTION_URL
+const DATABASE_URL = "mongodb+srv://admin:admin@stack-overflow-clone.p1irn7q.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology:true})
     .then(()=>app.listen(PORT, ()=>{console.log(`server running on port ${PORT}`)}))
     .catch((err)=>console.log(err.message))
